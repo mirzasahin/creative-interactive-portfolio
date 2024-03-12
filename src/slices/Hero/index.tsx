8d29763e2ae1fcc1b943b72cc8f6d65027a8a5d9
Mirza Sahin <salihmirza123@gmail.com>
2024-03-12 15:38:44 +0300

    First commit

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,26 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <span className="text-red-500">{slice.primary.firstname}</span>
+      <>{slice.primary.lastname}</>
+      <>{slice.primary.tagline}</>
+     
+    </section>
+  );
+};
+
+export default Hero;
