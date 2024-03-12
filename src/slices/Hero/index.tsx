32635b5972ec953e05cd0e648ac1e13d272ef5af
Mirza Sahin <salihmirza123@gmail.com>
2024-03-12 20:32:38 +0300

    Add Letters anim

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
@@ -18,17 +18,47 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
   useEffect(() => {
     let ctx = gsap.context(() => {
-      const tl = gsap.timeline()
+      const tl = gsap.timeline();
 
-      tl.fromTo(".name-animation",
-      {
-        x: -100, opacity:0, rotate: -10
-      },
-      
-      {
-        x: 0, opacity: 1, rotate: 0
-      }
-        )
+      tl.fromTo(
+        ".name-animation",
+        {
+          x: -100,
+          opacity: 0,
+          rotate: -10,
+        },
+
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "elastic.out(1,0.3)",
+          duration: 1,
+          transformOrigin: "left top",
+          delay: 0.5,
+          stagger: {
+            each: 0.1,
+            from: "random",
+          },
+        }
+      );
+
+      tl.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          scale: 1,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
     }, component);
     return () => ctx.revert();
   }, []);
@@ -60,11 +90,11 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <span className="block text-slate-300">
               {renderLetters(slice.primary.firstname, "first")}
             </span>
-            <span className="-mt-[0.2em] block text-slate-500">
+            <span className="-mt-[.2em] block text-slate-500">
               {renderLetters(slice.primary.lastname, "first")}
             </span>
           </h1>
-          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text4xl">
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
             {slice.primary.tagline}
           </span>
         </div>
