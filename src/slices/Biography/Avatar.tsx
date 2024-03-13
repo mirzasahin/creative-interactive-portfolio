25f6855c38ccdb55aa314ca7dcb5f31bbb1e5151
Mirza Sahin <salihmirza123@gmail.com>
2024-03-13 08:58:01 +0300

    Add about page title, desc and img

---
+++ src/slices/Biography/Avatar.tsx
@@ -0,0 +1,16 @@
+import { ImageField } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+
+type AvatarProps = {
+  image: ImageField;
+  className?: string;
+};
+
+export default function Avatar({ image, className }: AvatarProps) {
+  return (
+    <PrismicNextImage
+      field={image}
+      className="row-start-1 max-w-sm md:col-start-2 md:row-end-3 rounded-3xl"
+    />
+  );
+}
