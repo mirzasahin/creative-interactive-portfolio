db039464b69e1c860b69a23bbf1f3e637e487360
Mirza Sahin <salihmirza123@gmail.com>
2024-03-21 01:59:41 +0300

    Fix some issue

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -271,17 +271,6 @@ interface ProjectDocumentData {
   hover_image: prismic.ImageField<never>;
 
   /**
-   * Link field in *Project*
-   *
-   * - **Field Type**: Link
-   * - **Placeholder**: *None*
-   * - **API ID Path**: project.link
-   * - **Tab**: Main
-   * - **Documentation**: https://prismic.io/docs/field#link-content-relationship
-   */
-  link: prismic.LinkField;
-
-  /**
    * Slice Zone field in *Project*
    *
    * - **Field Type**: Slice Zone
